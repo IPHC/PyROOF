2cf136167369caeea960793ef869c83d332c4909
Alexandre Aubin <alexandre.aubin@cern.ch>
2015-03-15 01:01:52 +0100

    Adding possibility to run transparently on files directly on dpm. User may put 'local:' or 'dpm:' in front of paths of datasets

--- config/debuggingMode.py
+++ config/debuggingMode.py
@@ -7,5 +7,9 @@ import config
 # ##################
 
 def main() :
+
+    if (len(config.datasets) <= 0) :
+        print "Warning : no datasets given"
+
     config.processor(config.datasets[0].files[0], config.analyzer, "./debuggingOutput.tmp.root")
 
--- core/genericDataset.py
+++ core/genericDataset.py
@@ -1,12 +1,46 @@
 import glob
+import sys, subprocess
 
 #######################################################
 class Dataset:
 
     def __init__(self, name, wildcard, xsection = -1) :
         self.name        = name
-        self.files       = glob.glob(wildcard)
         self.xsection    = xsection
+        self.files       = [ ]
+
+        print "Listing files for dataset", self.name, "..."
+
+        # Getting dataset file list
+
+        if (wildcard.startswith("local:")) :
+            self.files       = glob.glob(wildcard.replace("local:",""))
+        elif (wildcard.startswith("dpm:")) :
+
+            # Path to use for rfdir (below)
+            dpmPath = wildcard.replace("dpm:","/dpm/in2p3.fr/home/cms/phedex/store/user/")
+            # Path to use for root_open (later)
+            xrdPath = wildcard.replace("dpm:","root://sbgse1.in2p3.fr//cms/phedex/store/user/")
+
+            cmd = "/usr/bin/rfdir", dpmPath
+
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            p.wait()
+            (out, err) = p.communicate()
+            for line in out.split('\n') :
+                fields = line.split()
+                if (len(fields) == 9) :
+                    self.files.append(xrdPath+fields[8])
+
+        else :
+            print "Wildcard for dataset ", self.name, "doesn't starts with '/' or 'root://'"
+
+        # Check files have been found
+
+        if (len(self.files) == 0) :
+            print "Error : found 0 files when initializing dataset "+self.name+"."
+            print "Exiting."
+            sys.exit(-1)
 
     def files(self) :
         return self.files
