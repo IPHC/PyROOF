6f5bd1ce9800cd5735c06ccc1d3e82bce6823335
Eric Chabert <Eric.Chabert@cern.ch>
2016-06-29 15:27:21 +0200

    New mode: run on a  dedicated file locally choosen at execution

--- PyROOF.py
+++ PyROOF.py
@@ -16,8 +16,11 @@ parser.add_option("-b", "--batch", dest="pbsMultiprocessingMode", default=False,
                           action="store_true", help="Batch/PBS multprocessing mode, launch one PBS job for each file to run on.")
 parser.add_option("-p", "--prof", dest="profMode", default=False,
                           action="store_true", help="Use cProfile on the Debug mode, meant to check if recent modifications in analysis code didn't break anything.")
+parser.add_option("-O", "--one", dest="oneDataset", default=False,
+			  action="store_true", help="Run on a single file locally")
 
 (options, args) = parser.parse_args()
+print args
 
 ############
 #  Launch  #
@@ -62,6 +65,20 @@ elif (options.profMode) :
     import pstats
     p = pstats.Stats('fooprof')
     print p.sort_stats(-1).print_stats()
+
+elif (options.oneDataset): 
+    print " Running locally on one dataset"
+    from config import oneDatasetLocalMode
+    if len(args) == 2:
+    	datasetname = args[0]
+        print " dataset = ", datasetname
+        print " "
+	filename = args[1]
+	oneDatasetLocalMode.main(datasetname,filename)
+    else:
+    	print " You need to give 2 additional information: "
+	print " 1) datasetName "
+	print " 2) the full root file path"
 
 else :
     print "No running mode selected, try --help"
